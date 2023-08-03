29f3da9c5706c5dcd230c1352e3ccbc02aa98419
thestackdev <shanmukeshwar03@gmail.com>
2023-08-03 19:49:33 +0530

    Update to edge runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { checkSignedIn } from "@/helpers/session";
 import { redirect } from "next/navigation";
 
+export const runtime = "edge";
+
 export default async function Page() {
   const payload = await checkSignedIn();
 
