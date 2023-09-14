7863626c717b17a1a2bc826da9b3ff3a3575b6a9
thestackdev <shanmukeshwar03@gmail.com>
2023-09-14 21:15:23 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
